1b03f2165125ca2bd619788a4c6c8c409f6ccd7d
Christian Stussak <christian.stussak@imaginary.org>
2025-03-11 15:26:30 +0100

    Simplify notifcation LED handling
    The LED will now switch state after each event. When an odd number of
    buttons is in the pressed state, the LED will be on. When an even number
    of buttons is in the pressed state, the LED will be off.
    
    This simplification also removed the need for the `asyncio` module.

--- code.py
+++ code.py
@@ -1,6 +1,5 @@
 import board
 import digitalio
-import asyncio
 import keypad
 import usb_hid
 
@@ -39,7 +38,7 @@ if config.led_pin is not None:
     led = digitalio.DigitalInOut(config.led_pin)
     led.switch_to_output(value=False)
 else:
-    led = None
+    led = {"value": False}
 
 print("Current configuration:")
 print("Keys:", config.keys)
@@ -49,34 +48,21 @@ print("Maximum poll interval:", interval)
 print("Digital LED output pin:", config.led_pin if hasattr(config, "led_pin") else None)
 
 
-async def blink(led):
-    async def blink_internal():
-        led.value = True
-        await asyncio.sleep(0.05)
-        led.value = False
-
-    asyncio.create_task(blink_internal())
-
-
-async def main():
+def main():
     with keypad.Keys(
         pins, value_when_pressed=config.value_when_pressed, pull=pull, interval=interval
     ) as keys:
         while True:
             event = keys.events.get()
             if event:
+                led.value = not led.value
                 keycodes = config.keys[event.key_number][1]
                 if event.pressed:
-                    if led is not None:
-                        await blink(led)
                     print("key(s) {} pressed".format(keycodes))
                     keyboard.press(*keycodes)
                 elif event.released:
-                    if led is not None:
-                        await blink(led)
                     print("key(s) {} released".format(keycodes))
                     keyboard.release(*keycodes)
-            await asyncio.sleep(0)
 
 
-asyncio.run(main())
+main()
